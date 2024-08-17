c9d1280ebe7d34fbe87000672d4213dcc14b950e
Yuri <yuri.salgado.ft@gmail.com>
2024-08-17 19:59:24 -0300

    testando no program.cs se retornou certo.

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -10,7 +10,7 @@ namespace DesafioPOO.Models
         // TODO: Sobrescrever o método "InstalarAplicativo"
         public override void InstalarAplicativo(string nomeApp)
         {
-            Console.WriteLine($"Instalando aplicativo {nomeApp}");
+            Console.WriteLine($"Instalando aplicativo {nomeApp} no Nokia.");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Testando o Nokia");
+var nokiaPhone = new Nokia("123456", "modelo 1", "111111", 32);
+nokiaPhone.Ligar();
+nokiaPhone.InstalarAplicativo("facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Testando o Iphone");
+var Iphone = new Nokia("7891011", "modelo 2", "222222", 64);
+Iphone.Ligar();
+Iphone.InstalarAplicativo("whatzapp");
