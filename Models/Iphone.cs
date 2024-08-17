e343c394aac2ccb08f6194cd98db0e30cedafa47
Yuri <yuri.salgado.ft@gmail.com>
2024-08-17 19:13:48 -0300

    adicionando a legenda da instalação no Iphone

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -10,7 +10,7 @@ namespace DesafioPOO.Models
         // TODO: Sobrescrever o método "InstalarAplicativo"
         public override void InstalarAplicativo(string nomeApp)
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"Instalando aplicativo {nomeApp}");
         }
     }
 }
